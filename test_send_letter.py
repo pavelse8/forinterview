2ccdebe39ca67d109b93a90325647572e8d4ff38
pavelse8 <pavelse8@gmail.com>
2020-01-07 01:17:20 +0300

    first commit

---
+++ test_send_letter.py
@@ -0,0 +1,17 @@
+import pytest
+from pages.login_page import LoginPage
+
+LINK_LOGIN_PAGE = "https://mail.ru/"
+
+
+@pytest.mark.login
+def test_authorize_for_rop(browser):
+    login_page = LoginPage(browser, LINK_LOGIN_PAGE)
+    login_page.open_page()
+    login_page.enter_the_login()
+    login_page.enter_the_password()
+    mailbox_page = login_page.go_to_mailbox_page()
+    mailbox_page.create_new_letter()
+    mailbox_page.fill_adress()
+    mailbox_page.fill_body()
+    mailbox_page.send_letter()
